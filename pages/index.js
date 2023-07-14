29b9e0013e705902c7a4244cfc38cd67c1fafe49
Thompson Raul <89136846+ThompsonRaul@users.noreply.github.com>
2023-07-14 13:40:20 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home (){
-    return <h1>Oi Samilinha, você é muito lindinha, se você me ama, dá uma risadinha!😎</h1>
+function Home() {
+  return (
+    <h1>
+      Oi Samilinha, você é muito lindinha, se você me ama, dá uma risadinha!😎
+    </h1>
+  );
 }
-
 export default Home;
