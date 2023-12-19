16e7e8a6fafbc11d79bd66ab6d5fa3301c88b950
Thompson Raul <89136846+ThompsonRaul@users.noreply.github.com>
2023-12-19 11:05:44 +0000

    Finaliza endpoint `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1+1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ status: "Tudo ótimo por aqui!" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersion.rows[0].server_version;
+
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnections.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const openConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [dbName],
+  });
+  const openConnectionsValue = openConnections.rows[0].count;
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        db_version: dbVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        open_connections: openConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test("GET to /api/v1/status deve retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const resBody = await response.json();
+
+  const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
+  expect(resBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(resBody.dependencies.database.db_version).toEqual("16.0");
+  expect(resBody.dependencies.database.max_connections).toEqual(100);
+  expect(resBody.dependencies.database.open_connections).toEqual(1);
 });
